9ec504e7844f7c8f1a14f6e912602f8f2b045107
yashsonawane <yash050219991@gmail.com>
2020-04-18 07:11:15 +0000

    enchilada: Initial Lucid Bringup
    Signed-off-by: yashsonawane <yash050219991@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/lucid_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    lucid_enchilada-user \
+    lucid_enchilada-userdebug \
+    lucid_enchilada-eng
